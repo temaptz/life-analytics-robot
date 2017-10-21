40c4277da06efaf6380a5384de225122062c7ed9
temaptz <temaptz@gmail.com>
2017-10-21 23:29:06 +0300

    Добавлен сбор и отправка данных

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 const CronJob = require('cron').CronJob;
 const request = require('request');
 const cheerio = require('cheerio');
-
-const url      = 'https://matbea.com';
-const selector = '.jsRateSellVal';
-const cronTime = '*/5 * * * * *';
+const config  = require('./config');
 
 const jobTask = () => {
-    console.log('tick');
 
-    request(url, (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            const text = $(selector);
-            console.log(text);
-        }
-    });
+    request.get(
+        {
+            url     : config.url,
+            headers : {
+                'User-Agent': config.userAgent
+            }
+        }, (err, resp, body) => {
+
+            if (!err && resp.statusCode == 200) {
+
+                const $ = cheerio.load(body);
+                const text = $(config.selector).text();
+                if ( text && text.length > 0 ) {
+                    const newValue = parseFloat(text);
+                    console.log(`Recieved new value: ${newValue}`);
+                    sendValueToLifeAnalytics(newValue);
+                }
+
+            }
+
+        });
 };
 
+// Отправка данных в life-analytics
+sendValueToLifeAnalytics = (newValue) => {
+    request.post(
+        {
+            url  : config.analyticsUrl,
+            body : JSON.stringify({value : newValue}),
+            headers: {
+                'Authorization': `GraphHash ${config.analyticsHash}`
+            }
+        }, (err, resp, body) => {
 
-const job = new CronJob(cronTime, jobTask);
+            if (!err && resp.statusCode == 200) {
+
+                console.log(`New value sent`);
+
+            } else {
+
+                console.log(`Error sending new value`);
+
+            }
+
+        });
+};
+
+const job = new CronJob(config.cronTime, jobTask);
 job.start();
